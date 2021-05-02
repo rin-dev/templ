b94c89f6ff345e08a60719f35a52b15bfd0de40a
rin-dev <rinsoftware@mail.ru>
2021-05-03 00:41:20 +0600

    feat(IDE): change settings int* reformat
    * change setting IDE
    * reformat code
    
    #2

--- apps/frontend/admin/src/app/app.module.ts
+++ apps/frontend/admin/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot([], {initialNavigation: 'enabled'}),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
--- apps/frontend/barman/src/app/app.component.spec.ts
+++ apps/frontend/barman/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to frontend-barman!'
+      'Welcome to frontend-barman!',
     );
   });
 });
