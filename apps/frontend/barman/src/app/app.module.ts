b4f907e8281450e9f34aa89c712aad039d215285
dev <developer@rinsoft.kz>
2021-05-04 18:54:35 +0600

    feat(scss): set import to app
    set import to app
    
    Closes #12

--- apps/frontend/barman/src/app/app.module.ts
+++ apps/frontend/barman/src/app/app.module.ts
@@ -2,13 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
+import {UiComponentModule} from "@autodomkst/ui-component";
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [BrowserModule, UiComponentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
