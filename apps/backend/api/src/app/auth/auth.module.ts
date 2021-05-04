b8fbedda7f88e43e7ac0cb1d5aacea6c3f6405f0
rin-dev <rinsoftware@mail.ru>
2021-05-05 02:18:05 +0600

    feat(api-auth): add auth module [WiP]
    * change auth.module.ts
    
    #14

--- apps/backend/api/src/app/auth/auth.module.ts
+++ apps/backend/api/src/app/auth/auth.module.ts
@@ -1,4 +1,36 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {UserService} from '../users/services/user.service';
+import {AuthService} from './services/auth.service';
+import {PasswordService} from './services/password.service';
+import {JwtModule} from '@nestjs/jwt';
+import {PassportModule} from '@nestjs/passport';
+import {environment} from '../../environments/environment';
+import {JwtStrategy} from './services/jwt.strategy';
+import {AuthResolver} from './resolver/auth.resolver';
 
-@Module({})
-export class AuthModule {}
+@Module({
+  imports: [
+    UserService,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      privateKey: environment.jwt.secret,
+      signOptions: {
+        expiresIn: environment.jwt.expiresIn,
+      },
+    }),
+  ],
+  providers: [
+    AuthService,
+    PasswordService,
+    JwtStrategy,
+    AuthResolver,
+  ],
+  exports: [
+    AuthService,
+    PassportModule,
+  ],
+})
+export class AuthModule {
+}
