82abc73b04b280e36e32f0d0f0492bced832a52a
rin-dev <rinsoftware@mail.ru>
2021-05-05 02:28:49 +0600

    feat(api): add resolver scalar & typeORM setting [WiP]
    * add resolvers: [resolverMap] => GraphQLModule
    * add entities: [UserEntity] => TypeOrmModule
    * remove providers: [AppResolver] => app.module.ts
    * change app.resolver.ts
    
    #14

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,26 +1,28 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { environment } from '../environments/environment';
-import { GraphQLModule } from '@nestjs/graphql';
-import { AppResolver } from './app.resolver';
-import { AuthModule } from './auth/auth.module';
+import {AppController} from './app.controller';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {environment} from '../environments/environment';
+import {GraphQLModule} from '@nestjs/graphql';
+import {AuthModule} from './auth/auth.module';
+import {resolverMap} from './app.resolver';
+import {UserEntity} from './users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...environment.connection,
+      entities: [UserEntity],
     }),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
-      context: ({ req }) => ({ req }),
+      context: ({req}) => ({req}),
       playground: true,
+      resolvers: [resolverMap],
     }),
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppResolver],
 })
 export class AppModule {
 }
